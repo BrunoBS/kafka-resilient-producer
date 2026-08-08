6c0e08c27c86644790271f3fc530857659e586ea
Bruno Barbosa <brunobsgt@gmail.com>
2026-08-08 17:40:22 -0300

    todo: fluxo pensando em eventos por mensagem

--- src/main/java/com/example/kafka/controller/PublishController.java
+++ src/main/java/com/example/kafka/controller/PublishController.java
@@ -4,7 +4,12 @@ import com.example.kafka.dto.KafkaPublishDTO;
 import com.example.kafka.service.PublishService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/kafka/publish")
@@ -17,14 +22,38 @@ public class PublishController {
     }
 
     @PostMapping
-    public ResponseEntity<String> publish( @Valid @RequestBody KafkaPublishDTO dto) {
-        publishService.publish(
-                dto.environment(),
-                dto.topic(),
-                dto.key(),
-                dto.message()
-        );
+    public ResponseEntity<String> publish(
+            @Valid @RequestBody List<KafkaPublishDTO> dtos) {
+/*
+                    Kafka-resilient-producer
+                         │
+                         │ recebe lista
+                         ▼
+     Para cada dto deverá chamar a api de eventos para publicar  o status da chave
+                  ┌─────────────┐
+                  │ PUBLISHING  │
+                  └──────┬──────┘
+                         │
+                     tenta Kafka
+                    /          \
+                   /            \
+        sucesso   /        erro  \
+                 │                │
+                 ▼                ▼
+             PUBLISHED           ERROR
 
+             P
+        */
+
+
+        dtos.forEach(dto -> {
+            publishService.publish(
+                    dto.environment(),
+                    dto.topic(),
+                    dto.key(),
+                    dto.message()
+            );
+        });
         return ResponseEntity
                 .accepted()
                 .body("Message accepted for processing");
